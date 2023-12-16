030977b4f70edf1cf2dcfd56bba779493246b490
Jason Llanes <llanezjaizon@gmail.com>
2023-12-15 22:46:55 -0800

    React practice some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [show, setShow] = useState(false);
 
   return (
     <div>
+      <NavBar />
       {show && <Alert text="Alerrtttt" onClose={() => setShow(false)} />}
       <Button
         text="Button"
